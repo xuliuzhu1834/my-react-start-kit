ad54a5d1df937c7809f5f7566cae9c2ba5a693da
xuliuzhu1834 <30547352@qq.com>
2017-03-21 09:58:52 +0800

    Update entry.jsx
    remove redux-armc tab-event-plugin

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import assign from 'object-assign';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import { hashHistory } from 'react-router';
 import { syncHistoryWithStore, routerReducer, routerMiddleware } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
-import { asyncMiddleware } from 'redux-amrc';
 
 // reduxers & sagas
 import rootReducer, { rootSaga } from './components';
 // main component
 import RootView from './components/rootView';
-
-// 优化React在桌面浏览器下click事件
-injectTapEventPlugin();
 
 // reducers
 const reducersWithRouter = combineReducers(
@@ -31,7 +26,6 @@ const store = createStore(
   compose(
     applyMiddleware(
       routerMiddleware(hashHistory),
-      asyncMiddleware,
     ),
     applyMiddleware(sagaMiddleware),
     (window.devToolsExtension && window.devToolsExtension()) || (f => f),
